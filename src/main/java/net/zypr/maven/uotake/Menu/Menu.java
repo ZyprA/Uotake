b9106c84625edc59c91b0ac14b01d1f40676a40c
Zypr <44808225+ZyprA@users.noreply.github.com>
2024-12-28 12:41:33 +0900

    分割

--- src/main/java/net/zypr/maven/uotake/Menu/Menu.java
+++ src/main/java/net/zypr/maven/uotake/Menu/Menu.java
@@ -18,51 +18,76 @@ import org.bukkit.inventory.meta.ItemMeta;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 public class Menu {
     public static void open(Player p, String id) {
-        p.closeInventory();
-        if (Objects.equals(id, "gameselect")) {
+    p.closeInventory();
+    switch (id) {
+        case "gameselect":
             p.openInventory(gameselect.get());
-        } else if (Objects.equals(id, "mainmenu")) {
+            break;
+        case "mainmenu":
             p.openInventory(mainmenu.get());
-        } else if (Objects.equals(id, "equipeditor")) {
+            break;
+        case "equipeditor":
             p.openInventory(equipeditor.get(p));
-        } else if (id.startsWith("equip.")) {
-            String[] params = id.split("\\.");
-            p.openInventory(equip.get(params[1] + "." + params[2], p));
-        } else if (id.startsWith("shop.")) {
-            String[] params = id.split("\\.");
-            if (params.length != 2) {
-                return;
+            break;
+        default:
+            if (id.startsWith("equip.")) {
+                handleEquipMenu(p, id);
+            } else if (id.startsWith("shop.")) {
+                handleShopMenu(p, id);
+            } else {
+                p.sendMessage(ChatColor.RED + "メニュー情報が存在しません．管理者に問い合わせてください");
             }
-            String title = String.valueOf(Uotake.config.get("display.shop." + params[1]));
-            List<String> weapons = Uotake.weaponbytype.getWeapons(params[1]);
-            List<ItemStack> itemStacks = new ArrayList<>();
-            String category = "null";
-            if (Objects.equals(params[1], "ar") || Objects.equals(params[1], "sg") || Objects.equals(params[1], "sr") || Objects.equals(params[1], "smg") || Objects.equals(params[1], "rl")) {
-                category = "main";
-            } else if (Objects.equals(params[1], "hg")) {
-                category = "sub";
-            }
-            for (String weapon : weapons) {
-                ItemStack im = new ItemStack(Weapon.getMaterial(weapon, category));
-                NBTAPI.addNBT(im, "action", "buyweapon@" + weapon);
-                ItemMeta itemMeta = im.getItemMeta();
-                if (itemMeta != null) {
-                    itemMeta.setCustomModelData(Weapon.getCmd(weapon, category));
-                    itemMeta.setDisplayName(Weapon.getName(weapon, category));
-                }
-                im.setItemMeta(itemMeta);
-                itemStacks.add(im);
-            }
-            Inventory inv = Bukkit.createInventory(new InvHolder(), (((weapons.size() - 1) / 9) + 1) * 9, "§8§l" + title);
-            InvLoader.load(inv, itemStacks);
-            p.openInventory(inv);
-        } else {
-            p.sendMessage(ChatColor.RED + "メニュー情報が存在しません．管理者に問い合わせてください");
-        }
+            break;
     }
+}
+
+private static void handleEquipMenu(Player p, String id) {
+    String[] params = id.split("\\.");
+    p.openInventory(equip.get(params[1] + "." + params[2], p));
+}
+
+private static void handleShopMenu(Player p, String id) {
+    String[] params = id.split("\\.");
+    if (params.length != 2) {
+        return;
+    }
+    String title = String.valueOf(Uotake.config.get("display.shop." + params[1]));
+    List<String> weapons = Uotake.weaponbytype.getWeapons(params[1]);
+    List<ItemStack> itemStacks = new ArrayList<>();
+    String category = getCategory(params[1]);
+
+    for (String weapon : weapons) {
+        ItemStack im = new ItemStack(Weapon.getMaterial(weapon, category));
+        NBTAPI.addNBT(im, "action", "buyweapon@" + weapon);
+        ItemMeta itemMeta = im.getItemMeta();
+        if (itemMeta != null) {
+            itemMeta.setCustomModelData(Weapon.getCmd(weapon, category));
+            itemMeta.setDisplayName(Weapon.getName(weapon, category));
+        }
+        im.setItemMeta(itemMeta);
+        itemStacks.add(im);
+    }
+    Inventory inv = Bukkit.createInventory(new InvHolder(), (((weapons.size() - 1) / 9) + 1) * 9, "§8§l" + title);
+    InvLoader.load(inv, itemStacks);
+    p.openInventory(inv);
+}
+
+private static String getCategory(String type) {
+    switch (type) {
+        case "ar":
+        case "sg":
+        case "sr":
+        case "smg":
+        case "rl":
+            return "main";
+        case "hg":
+            return "sub";
+        default:
+            return "null";
+    }
+}
 
 }
